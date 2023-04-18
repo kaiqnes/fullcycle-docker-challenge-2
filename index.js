3c6a9251f5b7f456e954cc903c8cecd1bd986158
Caique Nunes <kaiqnes@gmail.com>
2023-04-18 15:49:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+app.use(express.json());
+
+function generateRandomName() {
+  const adjectives = ['happy', 'sad', 'angry', 'funny', 'serious', 'silly', 'wise', 'confused', 'sleepy', 'awake'];
+  const nouns = ['cat', 'dog', 'bird', 'fish', 'rabbit', 'turtle', 'lion', 'tiger', 'bear', 'monkey'];
+  const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+  const adjectiveIndex = Math.floor(Math.random() * adjectives.length);
+  const nounIndex = Math.floor(Math.random() * nouns.length);
+  const colorIndex = Math.floor(Math.random() * colors.length);
+  const adjective = adjectives[adjectiveIndex];
+  const noun = nouns[nounIndex];
+  const color = colors[colorIndex]
+  const name = `${adjective}-${color}-${noun}`;
+  return name;
+}
+
+app.get('/', (req, res) => {
+    console.log(`anchor: [HOST ${process.env.DB_HOST}] [USER ${process.env.DB_USER}] [PASS ${process.env.DB_PASSWORD}] [DB ${process.env.DB_DATABASE}]`)
+  const name = req.query.name || generateRandomName();
+  connection.query('INSERT INTO people (name) VALUES (?)', [name], (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error inserting name');
+    } else {
+      connection.query('SELECT name FROM people', (error, results) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Error retrieving names');
+        } else {
+          const names = results.map(result => result.name);
+          res.json({ names });
+        }
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
